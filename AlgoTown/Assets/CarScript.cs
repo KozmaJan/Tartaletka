8229775ed740f2ca82567f8afc5b5b56f2a6535c
KozmaJan <jan.kozma2006@gmail.com>
2025-05-12 00:11:42 +0200

    Jezdi to

--- AlgoTown/Assets/CarScript.cs
+++ AlgoTown/Assets/CarScript.cs
@@ -1,58 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CarScript : MonoBehaviour
 {
-    public TileInfo target;
+    public Vector3 target;
     public int type; //police, firefighter, ambulance
     public float moveSpeed = 20f;
     public float speed = 1; //modifier multiplied by movespeed (depending on the road type)
     public Animator animator;
-    public List<TileInfo> waypoints = new List<TileInfo>();
+    //public List<Transform> waypoints = new List<Transform>();
     public Vector3 Dir = new Vector3(0f,0f,0f);
+    public Vector3Int Autocheck = new Vector3Int(0,0,0);
     public Rigidbody2D ownRb;
     public float distanceTraveled;
     public float fuel;
     private GameMaster gameMaster;
+    public Tilemap tilemap;
+    
+
     void Start()
     {
-        gameMaster = GameObject.Find("GameManager").GetComponent<GameMaster>();
+        tilemap = GameObject.Find("Terrain").GetComponent<Tilemap>();
         ownRb = gameObject.GetComponent<Rigidbody2D>();
-        foreach(GameObject waypoint in waypointsObjects){
-            if (waypoint.GetComponent<WaypointInfo>()){
-                waypoints.Add(waypoint.GetComponent<WaypointInfo>());
-            }
+        TileBase tile = tilemap.GetTile(tilemap.WorldToCell(this.gameObject.transform.position));
+        if (tile is VariableTile roadtile)
+        {
+           gameObject.transform.position = roadtile.leftlane + tilemap.CellToWorld(tilemap.WorldToCell(this.gameObject.transform.position));
+           NextWaypointAuto();
         }
-        NextWaypoint();
+       else{
+        Destroy(gameObject);
+       }
+        //Get the tile you are on
+        //Get the information which direction you are facing and on which lane you are
+        //Go to the next tile in that direction
     }
     // Update is called once per frame
     void Update()
     {
         if (target != null){
-        Dir = target.position.position - this.gameObject.transform.position;
+        Dir = target - this.gameObject.transform.position;
         Dir.Normalize();
-        ownRb.MovePosition(ownRb.position + new Vector2(Dir.x, Dir.y) * moveSpeed * Time.deltaTime *(1f-slow));
+        ownRb.MovePosition(ownRb.position + new Vector2(Dir.x, Dir.y) * moveSpeed * Time.deltaTime);
         distanceTraveled += (new Vector2(Dir.x, Dir.y) * moveSpeed * Time.deltaTime * speed).magnitude;
-        if((Vector3.Distance(target.position.position, gameObject.transform.position)<Vector3.Distance(new Vector3(0, 0, 0), Dir*moveSpeed *Time.deltaTime))){
-            NextWaypoint();
+        if((Vector3.Distance(target, gameObject.transform.position)<=Vector3.Distance(new Vector3(0, 0, 0), Dir*moveSpeed *Time.deltaTime))){
+            NextWaypointAuto();
         }
         transform.rotation = Quaternion.Euler(0, (Dir.x > 0) ? 0 : 180, 0);
         }
     }
-    void NextWaypoint(){
-        bool selected = false;
-        foreach(TileInfo waypoint in waypoints){
-                target = waypoint;
-                selected = true;
-            }
-        if (!selected){
-            target = null;
-        }
-    }
+    //void NextWaypoint(){
+    //    bool selected = false;
+    //    foreach(Transform waypoint in waypoints){
+    //            target = waypoint;
+    //            selected = true;
+    //        }
+    //    if (!selected){
+    //        target = null;
+    //    }
+    //}
     void NextWaypointAuto(){ //automatically follow the direction of the road it's on, choses randomly at interjections
-
-    }
+        TileBase tile = tilemap.GetTile(tilemap.WorldToCell(this.gameObject.transform.position + new Vector3(-0.5f, -0.25f, 0f)));//leftdown (-0.5, -0.25)
+        Debug.Log("Next");
+        if (tile is VariableTile roadtile)
+        {
+            target = roadtile.leftlane + tilemap.CellToWorld(tilemap.WorldToCell(this.gameObject.transform.position)+new Vector3Int(-1, 0, 0));
+        }
+        else{
+            Destroy(gameObject);
+        }
+     }
     public void Dock(){ //docks at the police staion/hospital, whatever
             Destroy(gameObject);
     }
---
+++ AlgoTown/Assets/VariableTile.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(menuName = "VinTools/Custom Tiles/Variable Tile")]
+public class VariableTile : Tile
+{
+  public Vector3Int leftDir = new Vector3Int(0,0,0);
+  public Vector3Int rightDir = new Vector3Int(0,0,0);
+  public Vector3 leftlane = new Vector3(0, 0, 0);
+  public Vector3 rightlane = new Vector3(0, 0, 0);
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        base.RefreshTile(position, tilemap);
+    }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        //get these values from the base class
+        base.GetTileData(position, tilemap, ref tileData);
+
+        tileData.sprite = GetSprite(position);
+    }
+
+
+    [Header("Tile block")]
+    public Vector2Int m_size = Vector2Int.one;
+    public Sprite[] m_Sprites;
+
+    public Sprite GetSprite(Vector3Int pos)
+    {
+        //check if array lenght matches the dimensions
+        if (m_Sprites.Length != m_size.x * m_size.y) return sprite;
+
+        //prevents the values to be negative
+        while (pos.x < m_size.x) { pos.x += m_size.x; }
+        while (pos.y < m_size.y) { pos.y += m_size.y; }
+
+        //get the index on each axis
+        int x = pos.x % m_size.x;
+        int y = pos.y % m_size.y;
+
+        //get the index in the array
+        int index = x + (((m_size.y - 1) * m_size.x) - y * m_size.x);
+
+        //returns the correct sprite
+        return m_Sprites[index];
+    }
+}
